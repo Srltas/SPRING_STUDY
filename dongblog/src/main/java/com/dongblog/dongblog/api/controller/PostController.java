ead9b24686922e895b27ecc0cf2bab1a5b000de2
CUBRID <kdm97912@gmail.com>
2022-07-15 00:35:53 +0900

    Validation 처리, @ControllerAdvice  예외처리 공통화

--- dongblog/src/main/java/com/dongblog/dongblog/api/controller/PostController.java
+++ dongblog/src/main/java/com/dongblog/dongblog/api/controller/PostController.java
@@ -1,24 +1,20 @@
 package com.dongblog.dongblog.api.controller;
 
-import com.dongblog.dongblog.api.controller.request.PostCreate;
+import com.dongblog.dongblog.api.request.PostCreate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.Map;
+
 @Slf4j
 @RestController
 public class PostController {
 
-    //SSR -> jsp, thymeleaf, mustache, freemarker -> html rendering
-    //SPA -> vue
-            // javascript로 화면 생성 + 서버와는 API(JSON)으로만 통신
-    //  vue -> vue+SSR = nuxt.js
-    //  react -> react+SSR = next.js
-
     @PostMapping("/posts")
-    public String post(@RequestBody PostCreate postCreate) {
-        log.info("postCreate={}", postCreate);
-        return "Hello World";
+    public Map<String, String> post(@RequestBody @Valid PostCreate postCreate) {
+        return Map.of();
     }
 }
--- dongblog/src/test/java/com/dongblog/dongblog/api/controller/PostControllerTest.java
+++ dongblog/src/test/java/com/dongblog/dongblog/api/controller/PostControllerTest.java
@@ -8,10 +8,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 class PostControllerTest {
@@ -22,17 +22,38 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청 시 Hello World를 출력한다.")
     void test() throws Exception {
-        //글 제목
-        //글 내용
-
-
-        //expected
         mockMvc.perform(post("/posts")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
                 )
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("/posts 요청 시 title 값이 비어있는 경우")
+    void test2() throws Exception {
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"\", \"content\": \"내용입니다.\"}")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("타이틀을 입력해주세요."))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("ErrorResponse를 만들어서 에러 처리를 공통적으로 처리")
+    void test3() throws Exception {
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": null, \"content\": \"내용입니다.\"}")
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
                 .andDo(print());
     }
 }
